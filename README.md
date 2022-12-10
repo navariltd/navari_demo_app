fcf4c6e7551abaf312ed2d870571fd4287de292f
Lucky-Tsuma <tsumahmumba@gmail.com>
2022-12-10 22:04:56 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Navari Demo App
+
+Demo application for Opportunity Sales Projections report
+
+#### License
+
+GNU General Public License (v3)
---
+++ navari_demo_app/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Navari Demo App",
+			"type": "module",
+			"label": _("Navari Demo App")
+		}
+	]
